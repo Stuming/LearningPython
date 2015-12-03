a3caa75a34eac793236e403bd75d9d9f46f7c411
sunshineDrizzle <954830460@qq.com>
2015-12-03 21:31:23 +0800

    initialize c05 practice

---
+++ c05/e04.py
@@ -0,0 +1,16 @@
+#问：通过什么工具你可以找到一个数字的平方根以及它的平方？
+#答：至少可以通过math模块，内置函数pow()以及指数表达式三种方法。
+#代码验证：
+import math
+X=2
+
+print("下面是三种方法求X的平方根的结果")
+print(math.sqrt(X))
+print(pow(X,0.5))
+print(X**0.5)
+print("下面是三种方法求X的平方的结果")
+print(math.pow(X,2))
+print(pow(X,2))
+print(X**2)
+
+input()
