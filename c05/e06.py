ae4a1b4f9122eb79144232ab88f48fec92d17949
jiangjiane <jiangjiane@outlook.com>
2015-12-04 10:35:04 +0800

    edit && modify c05

--- c05/e06.py
+++ c05/e06.py
@@ -1,6 +1,11 @@
-#问：怎样能够截断或舍去浮点数的小数部分？
-#答：用int()函数和math模块的trunc()方法可以截断小数部分；而round()方法是做四舍五
-#    入；math模块的floor()方法是舍去小数部分而得到离原数值最近且小于原数值的整数。
+#! /usr/bin/python3
+# -*- coding:utf8 -*-
+
+'''
+问：怎样能够截断或舍去浮点数的小数部分？
+答：用int()函数和math模块的trunc()方法可以截断小数部分；而round()方法是做四舍五入；math模块的floor()方法是舍去小数部分而得到离原数值最近且小于原数值的整数。
+'''
+
 #代码验证：
 import math
 X=-1.141
@@ -10,4 +15,4 @@ print("math.trunc(X):%f" % math.trunc(X))
 print("round(X):%f" % round(X))
 print("math.floor(X):%f" % math.floor(X))
 
-input()
+input() # 控制台下显示才使用这行，防止一闪而过看不到执行结果
