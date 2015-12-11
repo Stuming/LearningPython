ac1452f254700f49259405ec52fe2c36485d508f
sunshineDrizzle <954830460@qq.com>
2015-12-11 21:08:09 +0800

    edit c09/e05, e06.py

--- c09/e05.py
+++ c09/e05.py
@@ -2,7 +2,20 @@
 # -*- coding:utf8 -*-
 
 """
-你怎么看一次复制嵌套结构的所有组成部分？
+你怎么一次复制嵌套结构的所有组成部分？
 
 答：导入copy模块，然后调用copy.deepcopy()。实际中浅层复制通常足以满足大多数要求，如aList[:]、aDict.copy()。
 """
+
+#Example
+import copy
+L1=[1, 2, [3, 4]]
+L2=copy.deepcopy(L1)
+L3=L1.copy()
+
+print("原本的L1:", L1)
+print("L2, L3分别是L1的深层拷贝和浅层拷贝")
+L2[2][0]=3.5
+print("经L2[2][0]=3.5后的L1不变：", L1)
+L3[2][0]=3.5
+print("经L3[2][0]=3.5后的L1改变：", L1)
--- c09/e06.py
+++ c09/e06.py
@@ -8,15 +8,7 @@ Python在什么时候会认为一个对象为真？
 """
 
 #Example
-temp1 = 1
-temp2 = 0
-temp3 = [1,2,3]
-temp4 = []
-bool1 = bool(temp1)
-bool2 = bool(temp2)
-bool3 = bool(temp3)
-bool4 = bool(temp4)
-print ("temp1 = 1:",bool1)
-print ("temp2 = 0:",bool2)
-print ("temp3 = [1,2,3]:",bool3)
-print ("temp4 = []:",bool4)
+print ("bool(1):",bool(1))
+print ("bool(0):",bool(0))
+print ("bool([1, 2, 3]):",bool([1, 2, 3]))
+print ("bool([]):",bool([]))
