54afa83cdfd3061a022746cd5954e0432d951268
Stuming <1361046649@qq.com>
2015-12-16 17:22:30 +0800

    edit c10 e01,e06

--- c10/e01/e01.c
+++ c10/e01/e01.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
+int fun_max(int a,int b)
+{
+	if (a<b)
+		return b;
+	return a;
+}
+
 int main()
     {
-        int a,b,max;
+        int a,b;
         printf("input two numbers:\n");
         printf("a=");
         scanf("%d",&a);
         printf("b=");
         scanf("%d",&b);
-        max=a;
-        if (max<b)
-            max=b;
-        printf("max=%d\n",max );
+        printf("max=%d\n",fun_max(a,b) );
         return 0;
     }
