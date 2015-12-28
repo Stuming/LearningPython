7a9ea8d4b321fcb5ace3921240d48c64117e55eb
jiangjiane <jiangjiane@outlook.com>
2015-12-28 14:00:50 +0800

    add c22

---
+++ c22/e04.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding:utf8 -*-
+
+'''
+问：什么时候必须使用import，不能使用from？
+答：当需要读取两个不同模块中的相同变量名时，就必须使用import，而不能使用from；因为必须定制变量名所在模块，从而保证这两个变量名(如 module1.name和module2.name)是独特的。
+'''
