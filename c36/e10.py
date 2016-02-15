8be087e548f725923c482b2edde15f49319844b4
Stuming <1361046649@qq.com>
2016-02-15 12:57:40 +0800

    small edit

--- c36/e10.py
+++ c36/e10.py
@@ -2,10 +2,10 @@
 # -*- coding:utf8 -*-
 
 '''
-�ʣ�Python 3.0���ַ��������޸Ķ���Ĵ����ж���Ӱ�죿
-��Python 3.0���ַ��������޸ĵ�Ӱ�죬 ȡ��������ʹ�õ��ַ��������͡�
-    ������Щʹ�ü�ASCII�ı��Ľű������ܸ���û��Ӱ�죺�ڴ�����£�str�ַ���������Python 2.6��Python 3.0�е��÷���ͬ��
-    ���⣬���ܱ�׼������re�� struct�� pickle��xml�����ַ�����صĹ��߿�����Python 3.0��Python 2.6�м����ϵ��÷���ͬ������һ�޸ĺܴ�̶���������������ء�
-	��ΪPython 3.0��str��bytes�Լ�Python 2.6��str��֧�ּ�����ͬ�Ľӿڡ�����㴦����ASCII��Unicode���ݣ� ֻ��Ҫֱ�Ӱ�Python 2.6�Ĺ��߼�unicode��codecs.open()ת��ΪPython 3.0��str��open��
-	����Ǵ��������������ļ�������Ҫ������������Ϊbytes��bytearray��������ݡ�����������Python 2.6�ַ����������ƵĽӿڣ� ����Ӱ���ٴα�ú�С��
+问：Python 3.0的字符串类型修改对你的代码有多大的影响？
+答：Python 3.0的字符串类型修改的影响， 取决于你所使用的字符串的类型。
+    对于那些使用简单ASCII文本的脚本，可能根本没有影响：在此情况下，str字符串类型在Python 2.6和Python 3.0中的用法相同。
+    此外，尽管标准库中像re、 struct、 pickle和xml这样字符串相关的工具可能在Python 3.0和Python 2.6中技术上的用法不同，但这一修改很大程度上与大多数程序不相关。
+	因为Python 3.0的str和bytes以及Python 2.6的str都支持几乎相同的接口。如果你处理非ASCII的Unicode数据， 只需要直接把Python 2.6的工具集unicode和codecs.open()转换为Python 3.0的str和open。
+	如果是处理二进制数据文件，将需要处理的数据作为bytes或bytearray对象的内容。由于它们与Python 2.6字符串具有相似的接口， 所以影响再次变得很小。
 '''
